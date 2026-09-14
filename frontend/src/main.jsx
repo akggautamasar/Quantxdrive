a9b968404274b4b7833079c00a12838f1865104e
akggautamasar <Worksbeyondworks@gmail.com>
2026-09-14 22:12:10 +0530

    feat: mount Channels browser in QuantXDrive

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ChannelsLauncher from './ChannelsLauncher.jsx'
+
+function Root() {
+  const token = localStorage.getItem('ad_token') || ''
+  return <><App /><ChannelsLauncher token={token} /></>
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
